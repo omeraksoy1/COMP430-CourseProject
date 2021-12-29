7affc8984bfa057cbb037dcd6e092b0a4789d0fd
mkoksal <mertkoksaal@gmail.com>
2021-12-29 13:28:33 +0300

    Simple get request to backend

---
+++ frontend/src/app/button/button.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+  private url: string;
+  constructor(private http: HttpClient) {
+    this.url = 'http://localhost:8080/api/avg';
+  }
+
+
+      getAll() {
+          this.http.get(this.url).subscribe((results) => {
+            console.log('Data is received - Result - ', results);
+          })
+      }
+
+
+  ngOnInit(): void {
+  }
+
+}
